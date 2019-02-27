15744ca2565bbaf19db3f5a29dc9fc0239365df8
Jenz Arsen Alabado <jenzarsen.alabado@gmail.com>
2019-02-27 17:52:51 +0800

    Multiplayer Test v1.0.1

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class MoveCube : MoveCubeBehavior {
+
+    private void Update()
+    {
+        if(! networkObject.IsServer)
+        {
+            transform.position = networkObject.position;
+            return;
+        }
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"), transform.position.z)*Time.deltaTime*5f;
+
+        networkObject.position = transform.position;
+    }
+}
